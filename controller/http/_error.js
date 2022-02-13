25e85db75c6a27c8dd1429d1cf2f03b77d063cf9
Jeff Straney <straneyjeff@gmail.com>
2022-02-13 12:25:45 -0500

    v2.0.2 Fixed error handling across web/api
    Because of how express registers error handlers as global
    across routers, a central error handler in the project template
    was selected and switches were added for handling JSON
    responses vs a web response (no use in sending back HTML to
    an API request)

--- controller/http/_error.js
+++ controller/http/_error.js
@@ -2,7 +2,16 @@ const {
   useArgs,
   validateArgs,
   viewTemplate,
+  errorHandlers: {
+    normalizeError,
+  },
 } = require('@mazeltov/core/lib/middleware');
+
+const {
+  error: {
+    getHttpErrorStatusCode,
+  },
+} = require('@mazeltov/core/lib/util');
 
 module.exports = ( ctx = {} ) => {
 
@@ -11,19 +20,40 @@ module.exports = ( ctx = {} ) => {
     _useCSRF,
     _requireCSRF,
     loggerLib,
+    services: {
+      settingService: {
+        getSettings,
+      }
+    },
   } = ctx;
 
-  const logger = loggerLib(`${ctx.SERVICE_NAME}/controller/web/page`);
+  const [
+    appName
+  ] = getSettings([
+    'app.name',
+  ])
+
+  const logger = loggerLib(`${appName}/controller/http/_error`);
 
   const router = require('express').Router();
 
-  router.get('*', [
-    viewTemplate({
-      title: 'Page Not Found!',
-      template: 'error/_404',
-      logger,
-    }),
-  ]);
+  const _view404 = viewTemplate({
+    title: 'Page Not Found!',
+    template: 'error/_404',
+    logger,
+  });
+
+  router.get('/api/*', (req, res) => {
+      return res.status(404).json({
+        error: '_notFound',
+        message: 'This api route does not exist',
+        help: null,
+      });
+  });
+
+  router.get('*', (req, res) => {
+    return _view404(req, res);
+  });
 
   const _view50x = viewTemplate({
     title: 'Whoops',
@@ -35,6 +65,18 @@ module.exports = ( ctx = {} ) => {
   ctx.app.use((error, req, res, next) => {
 
     logger.error('%o', error);
+
+    if (/^\/api\//.test(req.path)) {
+
+      const code = getHttpErrorStatusCode(error);
+
+      if (process.env.NODE_ENV !== 'development') {
+        delete error.stack;
+      }
+
+      return res.status(code).json(error);
+
+    }
 
     res.locals.error = error;
 
